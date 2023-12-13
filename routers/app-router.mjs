46a7a5f33608668f8eca4735b45096ce2305f655
JayPatel <jay.patel1@gameskraft.com>
2023-12-13 19:23:31 +0530

    test

--- routers/app-router.mjs
+++ routers/app-router.mjs
@@ -4,5 +4,8 @@ import appController from '../controllers/app-controller.mjs';
 const router = express.Router();
 
 router.get('/scrape-data',appController.doScraping);
+router.get('/test',(req,res)=>{
+    res.status(200).json({ test : 'SuccessFully deployed' });
+});
 
 export default router;
